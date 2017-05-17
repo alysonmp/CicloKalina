c9d8a3e76e4fc3ebbcd239bbdd82251c58e2bc10
Alyson Pereira <alyson.pereira@beeleads.com.br>
2017-05-17 11:58:34 -0300

    Criadas mais 9 classes

--- src/Control/Ciclo2/ControlBomba.java
+++ src/Control/Ciclo2/ControlBomba.java
@@ -5,13 +5,15 @@
  */
 package Control.Ciclo2;
 
+import org.hibernate.Session;
+
 /**
  *
  * @author alysonmp
  */
 public class ControlBomba {
     
-    private double T9, P9, P10, H9, S9;
+    private double T9, P9, P10, H9, S9, T10s, S10, H10s, H10, T10;
     
     public ControlBomba(double Beff, double P1, double Pcon, double Tcon, double Pref, double Tref, double zi, Session session){
         T9 = Tcon;
@@ -24,22 +26,62 @@ public class ControlBomba {
         ControlS_SistemaMix SSistemamix = new ControlS_SistemaMix(T9, P9, Pref, Tref, zi, session);
         S9 = SSistemamix.getSL();
         
-        ControlIsoentropiaBomba isoBomba = new ControlIsoentropiaBomba();
+        ControlIsoentropiaBomba isoBomba = new ControlIsoentropiaBomba(Beff, P10, Pref, Tref, S9, H9, zi, T9, session);
+        T10s = isoBomba.getT10();
+        S10 = isoBomba.getS10();
+        
+        HSistemamix = new ControlH_Sistemamix(T10s, P10, Pref, Tref, zi, session);
+        H10s = HSistemamix.getHL();
+        H10 = ((H10s-H9)/Beff)+H9;
+        
+        T10 = T10s;
     }
-    
-}
 
-function [S9, H9, S10, H10, T10, P10] = Bomba(Beff, P1, Pcon, Tcon, Pref, Tref, zi)
-T9=Tcon;
-P9=Pcon;
-P10=P1;
-[HL9, HV9] = H_sistemamix(T9, P9, Pref, Tref, zi) ;
-H9=HL9;
-[SL9, SV9] = S_sistemamix(T9, P9, Pref, Tref, zi);
-S9=SL9;
-[T10s, S10] = isoentropiabomba(Beff, P10, Pref, Tref, S9, H9, zi, T9);
-[HL10, HV10] = H_sistemamix(T10s, P10, Pref, Tref, zi);
-H10s=HL10;
-H10=((H10s-H9)/Beff)+H9;
-T10=T10s;
-end
+    public double getP10() {
+        return P10;
+    }
+
+    public void setP10(double P10) {
+        this.P10 = P10;
+    }
+
+    public double getH9() {
+        return H9;
+    }
+
+    public void setH9(double H9) {
+        this.H9 = H9;
+    }
+
+    public double getS9() {
+        return S9;
+    }
+
+    public void setS9(double S9) {
+        this.S9 = S9;
+    }
+
+    public double getS10() {
+        return S10;
+    }
+
+    public void setS10(double S10) {
+        this.S10 = S10;
+    }
+
+    public double getH10() {
+        return H10;
+    }
+
+    public void setH10(double H10) {
+        this.H10 = H10;
+    }
+
+    public double getT10() {
+        return T10;
+    }
+
+    public void setT10(double T10) {
+        this.T10 = T10;
+    }    
+}
