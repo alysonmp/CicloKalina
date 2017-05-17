f79ade3bb81f5565cd21715b786214d2ba3828b9
Alyson Pereira <alyson.pereira@beeleads.com.br>
2017-05-17 16:34:55 -0300

    classes HIdeal_Gas, H_Sistema, S_Ideal_Gas e S_Sistema criadas

---
+++ src/Control/Ciclo2/ControlH_Ideal_Gas.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control.Ciclo2;
+
+import Model.ModelConstantesMat;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author alysonmp
+ */
+public class ControlH_Ideal_Gas {
+ 
+    private double Cp, Hig;
+    private int ii;
+    
+    public ControlH_Ideal_Gas(double T, double Tref, double x, Session session){
+        
+        Criteria cr = session.createCriteria(ModelConstantesMat.class);
+        List results = cr.list();
+        
+        //UTILIZA A PRIMEIRA LINHA BUSCADA, VARIÁVEL CC
+        ModelConstantesMat constantesMat = (ModelConstantesMat)results.get(2); 
+        double[][] valores = constantesMat.getValores();
+        
+        if(x == 0){
+            ii=0;
+        }else{
+            //%temperatura de 100K até 1500K
+            ii = 1;
+        }
+        
+        //Cp=cc(ii,1)+cc(ii,2)*T+cc(ii,3)*T^2+cc(ii,4)*T^3+cc(ii,5)*T^4;
+        Cp = valores[ii][0] + valores[ii][1] * T + valores[ii][2] * Math.pow(T, 2) + valores[ii][3] * Math.pow(T, 3) + valores[ii][4] * Math.pow(T, 4);
+        //Hig=((cc(ii,1)*T)+(cc(ii,2)*(T^2)/2)+(cc(ii,3)*(T^3)/3)+(cc(ii,4)*(T^4)/4)+(cc(ii,5)*(T^5)/5))-((cc(ii,1)*Tref)+(cc(ii,2)*(Tref^2)/2)+(cc(ii,3)*(Tref^3)/3)+(cc(ii,4)*(Tref^4)/4)+(cc(ii,5)*(Tref^5)/5));
+        Hig = ((valores[ii][0]*T)+(valores[ii][1]*(Math.pow(T,2))/2)+(valores[ii][2]*(Math.pow(T, 3))/3)+(valores[ii][3]*(Math.pow(T,4))/4)+(valores[ii][4]*(Math.pow(T, 5))/5))-((valores[ii][0]*Tref)+(valores[ii][1]*(Math.pow(Tref, 2))/2)+(valores[ii][2]*(Math.pow(Tref,3))/3)+(valores[ii][3]*(Math.pow(Tref,4))/4)+(valores[ii][4]*(Math.pow(Tref, 5))/5));
+    }   
+
+    public double getHig() {
+        return Hig;
+    }
+
+    public void setHig(double Hig) {
+        this.Hig = Hig;
+    }
+}
---
+++ src/Control/Ciclo2/ControlH_Sistema.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control.Ciclo2;
+
+import org.hibernate.Session;
+
+/**
+ *
+ * @author alysonmp
+ */
+public class ControlH_Sistema {
+    
+    private double HDrefL, HDrefV, Hig, HDL, HDV, HL, HV;
+    
+    public ControlH_Sistema(double T, double P, double Pref, double Tref, double x, Session session){
+        ControlH_Dep Hdep = new ControlH_Dep(Tref, Pref, x, session);
+        HDrefL = Hdep.getHDL();
+        HDrefV = Hdep.getHDV();
+        
+        ControlH_Ideal_Gas HIdealgas = new ControlH_Ideal_Gas(T, Tref, x, session);
+        Hig = HIdealgas.getHig();
+
+        Hdep = new ControlH_Dep(T, P, x, session);
+        HDL = Hdep.getHDL();
+        HDV = Hdep.getHDV();
+                
+        HL = -HDrefL+Hig+HDL;
+        HV = -HDrefL+Hig+HDV;
+
+    }
+
+    public double getHL() {
+        return HL;
+    }
+
+    public void setHL(double HL) {
+        this.HL = HL;
+    }
+
+    public double getHV() {
+        return HV;
+    }
+
+    public void setHV(double HV) {
+        this.HV = HV;
+    }
+}
