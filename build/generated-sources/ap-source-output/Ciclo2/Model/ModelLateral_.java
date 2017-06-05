4a338f0f29dfd12db980f38635b6d2b283a2fe29
Alyson Pereira <alyson.pereira@beeleads.com.br>
2017-06-05 09:32:49 -0300

    mostrar imagem ao abrir o programa

---
+++ build/generated-sources/ap-source-output/Ciclo2/Model/ModelLateral_.java
@@ -0,0 +1,25 @@
+package Ciclo2.Model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(ModelLateral.class)
+public abstract class ModelLateral_ {
+
+	public static volatile SingularAttribute<ModelLateral, Double> massa;
+	public static volatile SingularAttribute<ModelLateral, Double> pinch;
+	public static volatile SingularAttribute<ModelLateral, String> tipoTemp;
+	public static volatile SingularAttribute<ModelLateral, String> tipoMassa;
+	public static volatile SingularAttribute<ModelLateral, Double> pressao;
+	public static volatile SingularAttribute<ModelLateral, String> tipoTempCond;
+	public static volatile SingularAttribute<ModelLateral, Integer> ID;
+	public static volatile SingularAttribute<ModelLateral, Double> temperatura;
+	public static volatile SingularAttribute<ModelLateral, Double> efetividade;
+	public static volatile SingularAttribute<ModelLateral, Double> Superaquecimento;
+	public static volatile SingularAttribute<ModelLateral, Double> tempCond;
+	public static volatile SingularAttribute<ModelLateral, String> tipoPressao;
+
+}
+
